12aef9bac749a2c40b0744d3bd90cc13a48de2a0
李威 <liwei@qq.com>
2024-11-27 09:41:40 +0800

    修改IP为127.0.0.1

--- src/webui.py
+++ src/webui.py
@@ -21,7 +21,7 @@ def main():
     gradio_ipv6 = os.getenv("GRADIO_IPV6", "0").lower() in ["true", "1"]
     gradio_share = os.getenv("GRADIO_SHARE", "0").lower() in ["true", "1"]
     server_name = os.getenv("GRADIO_SERVER_NAME", "[::]" if gradio_ipv6 else "0.0.0.0")
-    create_ui().queue().launch(share=gradio_share, server_name=server_name, inbrowser=True)
+    create_ui().queue().launch(share=gradio_share, server_name="127.0.0.1", inbrowser=True)
 
 
 if __name__ == "__main__":
